6e5c089c013cae099bf657911cb6b4a96f1692f2
Nwlcnf <153742803+Nwlcnf@users.noreply.github.com>
2025-08-14 22:39:25 +0200

    feat: add CI/CD

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,21 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  moduleDirectories : ['node_modules', '<rootDir>'],
-  testMatch:['**/+(*.)+(spec).+(ts)'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text', 'lcov'],
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
+
 export default config;
